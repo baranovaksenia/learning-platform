6207dff943e0b8869c443a6e0b75d5b5c6552e64
Ksenia <baranova-ksenia1990@gmail.com>
2023-09-20 09:29:58 +0300

    add create course page and prisma schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import { ToasterProvider } from '@/components/providers/toaster-provider'
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,7 +18,10 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang='en'>
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<ToasterProvider />
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	)
